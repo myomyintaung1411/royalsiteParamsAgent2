b6319a2a6a09831931b4f4e41d6befa416b1f22a
myomyintaung1411 <myomyintaung1411@gmail.com>
2024-08-02 15:46:44 +0800

    lasted

--- public/unknow.js
+++ public/unknow.js
@@ -3,7 +3,7 @@ const xxzz = {
     old_pc_game_url:'https://pc1.hjbc666.com/',
     new_pc_game_url:'https://pc2.hj812.vip/',
     h5_game_url:'https://h5.hj812.vip/',
-    ad:"https://ht.hjzbc666.com/",
+    ad:"https://ht.hjbc666.com/",
     app_down:"https://download.hjbc66.com/",
     chat_app:"http://www.cx6699.top/",
     inv_code:"DN01093"
